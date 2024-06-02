360cce060c318dcdd9835d7c75455b52c657c137
hamza079 <hamzamassir9@gmail.com>
2024-06-02 18:16:51 +0100

    FMA

--- data/index.ts
+++ data/index.ts
@@ -1,217 +1,240 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+	{ name: 'About', link: '#about' },
+	{ name: 'Projects', link: '#projects' },
+	{ name: 'Testimonials', link: '#testimonials' },
+	{ name: 'Contact', link: '#contact' }
 ];
 
 export const gridItems = [
-  {
-    id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
-  },
-  {
-    id: 2,
-    title: "I'm very flexible with time zone communications",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-center",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 4,
-    title: "Tech enthusiast with a passion for development.",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
-  },
+	{
+		id: 1,
+		title: 'Tactics define success and strategy in football ',
+		description: '',
+		className:
+			'lg:col-span-3 text-white md:col-span-6 md:row-span-4 lg:min-h-[60vh]',
+		imgClassName: 'w-full h-full',
+		titleClassName: 'justify-end',
+		img: '/b11.jpg',
+		spareImg: ''
+	},
+	{
+		id: 2,
+		title: 'Global research in  football.',
+		description: '',
+		className: 'lg:col-span-2 text-white md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-start',
+		img: '',
+		spareImg: ''
+	},
+	{
+		id: 3,
+		title: 'Technology',
+		description: 'Performance in football transform with',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-center',
+		img: '',
+		spareImg: ''
+	},
+	{
+		id: 4,
+		title: 'Ensuring ease of accessibility',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-start',
+		img: '/grid.svg',
+		spareImg: '/b4.svg'
+	},
 
-  {
-    id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
-  },
-  {
-    id: 6,
-    title: "Do you want to start a project together?",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
-  },
+	{
+		id: 5,
+		title: 'Enhances our football analysis capabilities',
+		description: 'AI significantly',
+		className: 'md:col-span-3 md:row-span-2',
+		imgClassName: 'absolute right-0 bottom-0 md:w-[750px] w-60',
+		titleClassName: 'justify-center md:justify-start lg:justify-start',
+		img: '/pitchR.png',
+		spareImg: '/grid.svg'
+	},
+	{
+		id: 6,
+		title: 'Do you want to start an integration?',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+		img: '',
+		spareImg: ''
+	}
 ];
 
 export const projects = [
-  {
-    id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
-  },
-  {
-    id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+	{
+		id: 1,
+		title: 'Object Detection',
+		des: 'Detecting and analyzing players and other objects in video accurately.',
+		img: '/anal.png',
+		iconLists: [
+			'/numpy-1.svg',
+			'/32px-PyTorch_logo_icon.svg.png',
+			'/c.png',
+			'/cv.png',
+			'/pandas.svg'
+		],
+		link: 'Step_1'
+	},
+	{
+		id: 2,
+		title: 'Tracking objects in video',
+		des: 'Tracking objects in real-time video with high accuracy.',
+		img: '/anal.png',
+
+		iconLists: [
+			'/numpy-1.svg',
+			'/32px-PyTorch_logo_icon.svg.png',
+			'/c.png',
+			'/cv.png',
+			'/pandas.svg'
+		],
+		link: 'Step_2'
+	},
+	{
+		id: 3,
+		title: 'Collecting data',
+		des: 'Data collection and analysis for better insights.',
+		img: '/anal.png',
+
+		iconLists: [
+			'/numpy-1.svg',
+			'/32px-PyTorch_logo_icon.svg.png',
+			'/c.png',
+			'/cv.png',
+			'/pandas.svg'
+		],
+		link: 'Step_3'
+	},
+	{
+		id: 4,
+		title: 'Visualizing data',
+		des: 'Generate KPIs and graphs for better understanding.',
+		img: '/anal.png',
+
+		iconLists: [
+			'/numpy-1.svg',
+			'/32px-PyTorch_logo_icon.svg.png',
+			'/c.png',
+			'/cv.png',
+			'/pandas.svg'
+		],
+		link: 'Step_4'
+	}
 ];
 
 export const testimonials = [
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
+	{
+		quote: "Using this football match analysis app has revolutionized our training sessions. The AI-powered insights are incredibly accurate and have helped us pinpoint areas for improvement with ease. It's like having a professional analyst at our fingertips.",
+		name: 'David Roberts',
+		title: 'Head Coach of BlueWave FC'
+	},
+	{
+		quote: 'The AI in this app is a game-changer. It provides detailed breakdowns and tactical analyses that were previously only available to top-tier teams. Our players have benefited immensely from the feedback and visualizations.',
+		name: 'Emma Thompson',
+		title: 'Assistant Manager at Greenfield United'
+	},
+	{
+		quote: "This app is fantastic for post-match reviews. The AI-powered analysis gives us an edge by identifying patterns and trends that are easy to miss. It's become an essential tool for our coaching staff.",
+		name: 'John Mills',
+		title: 'Technical Director of RedStar Athletic'
+	},
+	{
+		quote: "I was impressed by the app's ability to deliver precise and actionable insights. The AI-driven recommendations have made our training sessions more focused and effective. Highly recommend it to any serious team.",
+		name: 'Sarah Edwards',
+		title: 'Performance Analyst at SilverLake Rangers'
+	},
+	{
+		quote: "Integrating this app into our match preparation routine has been a huge success. The AI capabilities provide us with a comprehensive understanding of both our own performance and that of our opponents. It's an invaluable resource.",
+		name: 'Mark Johnson',
+		title: 'Team Captain of Thunderbolt FC'
+	}
 ];
 
 export const companies = [
-  {
-    id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
-  },
-  {
-    id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
-  },
+	{
+		id: 1,
+		name: 'cloudinary',
+		img: '/cloud.svg',
+		nameImg: '/cloudName.svg'
+	},
+	{
+		id: 2,
+		name: 'appwrite',
+		img: '/app.svg',
+		nameImg: '/appName.svg'
+	},
+	{
+		id: 3,
+		name: 'HOSTINGER',
+		img: '/host.svg',
+		nameImg: '/hostName.svg'
+	},
+	{
+		id: 4,
+		name: 'stream',
+		img: '/s.svg',
+		nameImg: '/streamName.svg'
+	},
+	{
+		id: 5,
+		name: 'docker.',
+		img: '/dock.svg',
+		nameImg: '/dockerName.svg'
+	}
 ];
 
 export const workExperience = [
-  {
-    id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
+	{
+		id: 1,
+		title: 'Data Collection',
+		desc: 'Spearheaded the collection of extensive datasets, ensuring high accuracy and relevance for analysis.',
+		className: 'md:col-span-2',
+		thumbnail: '/1.svg'
+	},
+	{
+		id: 2,
+		title: 'Computer Vision',
+		desc: 'Designed and implemented computer vision algorithms to enhance image and video analysis capabilities.',
+		className: 'md:col-span-2',
+		thumbnail: '/2.svg'
+	},
+	{
+		id: 3,
+		title: 'Data Visualizations',
+		desc: 'Led the creation of interactive data visualizations to convey complex insights effectively.',
+		className: 'md:col-span-2',
+		thumbnail: '/3.svg'
+	},
+	{
+		id: 4,
+		title: 'Data Analysis',
+		desc: 'Conducted comprehensive data analysis to drive informed decision-making and strategic planning.',
+		className: 'md:col-span-2',
+		thumbnail: '/4.svg'
+	}
 ];
 
 export const socialMedia = [
-  {
-    id: 1,
-    img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
-    img: "/link.svg",
-  },
+	{
+		id: 1,
+		img: '/git.svg'
+	},
+	{
+		id: 2,
+		img: '/twit.svg'
+	},
+	{
+		id: 3,
+		img: '/link.svg'
+	}
 ];
